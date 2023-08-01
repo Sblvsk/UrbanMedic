a5be3c8d48d638434ee7a12b01176844975ca2e4
Sergey <smr.kolesnikov@gmail.com>
2023-08-01 11:01:36 +0500

    models

--- medic/models.py
+++ medic/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Exercise(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    periodicity = models.PositiveIntegerField()
+    doctors = models.ManyToManyField(Doctor, related_name='exercise_doctors')
+
+

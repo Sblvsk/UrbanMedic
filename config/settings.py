2007236e181fdeea87a57e9a56ce725d0852e4a0
Sergey <smr.kolesnikov@gmail.com>
2023-08-01 14:37:23 +0500

    settings for dev

--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project',
-        'USER': 'sergey',
-        'PASSWORD': 'sergey12',
-        'HOST': 'db',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project",
+        "USER": "sergey",
+        "PASSWORD": "sergey12",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 # Password validation
